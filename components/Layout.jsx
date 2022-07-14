11a826562b3426d8f6bcc8e5bdf7374f228d5cdd
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-14 22:57:54 +0530

    changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,8 +4,21 @@ import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,17 @@ import { Cart } from "./";
 import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Hurricane Electronics</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
