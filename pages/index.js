a98507a42af60ccc21fc06858cb3b6b3e6d45c3c
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-14 15:37:58 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { HeroBanner, Footer } from "../components";
 
 const Home = () => {
-  return <div>Index</div>;
+  return (
+    <>
+      <HeroBanner />
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of Many Products</p>
+      </div>
+      <div>
+        {["Product1", "Product2", "Product3"].map((product) => product)}
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
-<>
-  HeroBanner
-  <div>
-    <h2>Best Selling Products</h2>
-    <p>Speakers of Many Products</p>
-  </div>
-  <div>{["Product1", "Product2", "Product3"].map((product) => product)}</div>
-</>;
